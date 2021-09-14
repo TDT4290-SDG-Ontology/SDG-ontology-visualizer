53bc707538a9d1c60452961b666a550d66f73483
espandre <espandre@gmail.com>
2021-09-14 15:53:12 +0200

    Improve post test for api

--- utils/test_post/test.py
+++ utils/test_post/test.py
@@ -1,11 +1,12 @@
 import requests
 import sys
+import json
 
 URL = "http://localhost:3001/api"
 def login(username, password):
 	req = requests.post(URL + "/auth/login", json={'username': username, 'password': password })
 	print(req.status_code, req.reason)
-	print(req.text[:512])
-	return req.text
+	print(req.text)
+	return json.loads(req.text)
 
-login(sys.argv[1], sys.argv[2])
+body = login(sys.argv[1], sys.argv[2])
