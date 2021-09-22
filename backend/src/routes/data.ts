2c919c6ea1d94e13f02b0d9a72dfc117240a5d74
espandre <espandre@gmail.com>
2021-09-22 16:01:14 +0200

    Cleanup and some additional error handling.

--- backend/src/routes/data.ts
+++ backend/src/routes/data.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response } from 'express';
-// import { verifyRequestQueryParams } from '../common/router';
 import setData from '../database/setData';
 import getDataSeries from '../database/getDataSeries';
 import u4sscKpiMap from '../database/u4sscKpiMap';
@@ -15,18 +14,22 @@ const insertData = async (req: Request, res: Response) => {
     const year = parseInt(req.body.year, 10);
     if (Number.isNaN(year)) throw new ApiError(400, 'Year not an int.');
 
+    const indicatorName:string | undefined = u4sscKpiMap.get(req.body.indicator);
+    if (indicatorName === undefined || !(typeof indicatorName === "string"))
+      throw new ApiError(400, 'Unknown indicator');
+
     const newDataPoint = {
       indicatorId: req.body.indicator,
-      indicatorName: u4sscKpiMap[req.body.indicator],
+      indicatorName,
       municipality: req.body.municipality,
       data: req.body.data,
       year,
       isDummy: req.body.isDummy !== undefined && req.body.isDummy,
       dataseries: req.body.dataseries,
     };
-    console.log(newDataPoint.indicatorName);
-    const data = await setData(newDataPoint);
-    console.log(data);
+
+    const promise = await setData(newDataPoint);
+    res.status(200).json({});
   } catch (e: any) {
     onError(e, req, res);
   }
@@ -41,7 +44,7 @@ const getData = async (req: Request, res: Response) => {
   }
 };
 
-router.post('/insert', verifyToken, verifyDatabaseAccess, insertData);
+router.post('/insert', verifyDatabaseAccess, verifyToken, insertData);
 router.post('/get', verifyDatabaseAccess, getData);
 
 export default router;
