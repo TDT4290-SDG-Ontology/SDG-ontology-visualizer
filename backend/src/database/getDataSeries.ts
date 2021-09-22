c7138437b56f01dcf074e4918f909e58231d0e95
espandre <espandre@gmail.com>
2021-09-22 15:59:01 +0200

    Remove debug-logging

--- backend/src/database/getDataSeries.ts
+++ backend/src/database/getDataSeries.ts
@@ -4,8 +4,6 @@ import getDataSeries from './queries/getDataSeries';
 // TODO: Remove any
 export default async (kpiNumber: string): Promise<any> => {
   const query = getDataSeries(kpiNumber);
-  console.log('Query: ', query);
   const response = await DB.query(query, { transform: 'toJSON' });
-  console.log(response);
   return response;
 };
