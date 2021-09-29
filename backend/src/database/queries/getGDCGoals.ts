fff45bfd1e44a768da773b21f3a4aa9840030302
espandre <espandre@gmail.com>
2021-09-29 19:01:42 +0200

    Start of GDC goals queries.

---
+++ backend/src/database/queries/getGDCGoals.ts
@@ -0,0 +1,25 @@
+import { parsePrefixesToQuery } from '../../common/database';
+import { PREFIXES } from '../index';
+
+export default (municipality: string): string => {
+  const prefixString = parsePrefixesToQuery(PREFIXES.SDG, PREFIXES.SCHEMA, PREFIXES.RDFS);
+
+  return `
+        ${prefixString}
+        SELECT ?kpi ?baseline ?baselineYear ?target ?deadline ?minValue
+        WHERE {
+            ?indicator rdf:type SDG:U4SSCIndicator.
+            ?indicator SDG:kpiNumber ?kpi.
+            
+            ?goal rdf:type SDG:U4SSCIndicatorGoal.
+            ?goal SDG:goalBaseline ?baseline.
+            ?goal SDG:goalBaselineYear ?baselineYear.
+            ?goal SDG:goalDeadline ?deadline.
+            ?goal SDG:goalTarget ?target.
+
+            ?goal SDG:isGoalForMunicipality ?municipality.
+            ?goal SDG:isGoalForIndicator ?indicator.
+
+            ?municipality SDG:municipalityCode "${municipality}".
+        }`;
+};
