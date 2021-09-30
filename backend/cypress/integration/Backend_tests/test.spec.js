be7c0adb8092093cb04a2e4f11f887a0a1e1f16b
hudaal <hadhoood95@hotmail.com>
2021-09-30 13:40:53 +0200

    Installed Cypress in the backend, and wrote some tests in cypress/integration/Backend_tests

---
+++ backend/cypress/integration/Backend_tests/test.spec.js
@@ -0,0 +1,80 @@
+describe('Run backend', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3001')
+    });
+});
+
+describe('Login test with invalid user', () => {
+    it('POST', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3001/api/auth/login',
+            failOnStatusCode: false,
+            body: {
+                "username": "test1",
+                "password": "1234"
+            }
+        }).then((response) => {
+            //expect(response.status).equal(500)
+            expect(response.body.message).have.string('Invalid')
+        });
+    });
+});
+
+let token = '';
+describe('Login test with valid user', () => {
+    it('POST', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3001/api/auth/login',
+            body: {
+                "username": "test",
+                "password": "123"
+            }
+        }).then((response) => {
+            expect(response.status).equal(200)
+            expect(response.body).have.property('token')
+            token = response.body.token;
+        });
+    });
+});
+
+describe('Insertion test with valid values', () => {
+    it('POST', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3001/api/data/insert',
+            body: {"indicator": "EC: ICT: ICT: 1C",
+                "municipality": "Trondheim",
+                "data": "1",
+                "dataseries": "dataseries",
+                "year": "2020",
+                "isDummy": true,
+                "token": token
+            }
+        }).then((response) => {
+            expect(response.status).equal(200)
+        });
+    });
+})
+
+describe('Insertion test with invalid values', () => {
+    it('POST', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3001/api/data/insert',
+            failOnStatusCode: false,
+            body: {"indicator": "EC: ICT: 1C",
+                "municipality": "Trondheim",
+                "data": "1",
+                "dataseries": "dataseries",
+                "year": "2020",
+                "isDummy": true,
+                "token": token
+            }
+        }).then((response) => {
+            expect(response.status).equal(500)
+            expect(response.body.message).have.string('Unknown indicator')
+        });
+    });
+})
