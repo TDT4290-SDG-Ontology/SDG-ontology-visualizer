6bc7ddc4cb57a32f0c11e013df6d334ea9abd34c
Fredrik Busklein <fredrbus@stud.ntnu.no>
2021-10-01 13:55:33 +0200

    Added /isAlive endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,6 +6,10 @@ import data from './data';
 
 const router = Router();
 
+router.get('/isAlive', (req, res)=>{
+    res.send("true")
+})
+
 router.use('/ontologies', ontologies);
 router.use('/auth', authorization);
 router.use('/data', data);
