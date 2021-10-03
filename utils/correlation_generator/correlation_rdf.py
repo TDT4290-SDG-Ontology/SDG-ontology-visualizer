ddfa286dd52e56de103f6467bf70ca2c1adbfb5e
espandre <espandre@gmail.com>
2021-10-03 15:54:13 +0200

    Small usability improvement for correlation generator.

--- utils/correlation_generator/correlation_rdf.py
+++ utils/correlation_generator/correlation_rdf.py
@@ -1,3 +1,5 @@
+import sys
+
 import correlations
 
 def generate_correlation_rdf(cc):
@@ -17,7 +19,7 @@ def generate_correlation_rdf(cc):
         <rdf:type rdf:resource="http://www.semanticweb.org/aga/ontologies/2017/9/SDG#SDGTargetCorrelation"/>
         <SDG:subgoalCorrelationFrom rdf:resource="http://metadata.un.org/sdg/{from_target}"/>
         <SDG:subgoalCorrelationTo rdf:resource="http://metadata.un.org/sdg/{to_target}"/>
-        <SDG:subgoalCorrelationCountry>{cc}</SDG:subgoalCorrelationCountry>
+        <SDG:subgoalCorrelationCountry rdf:datatype="http://www.w3.org/2001/XMLSchema#string">{cc}</SDG:subgoalCorrelationCountry>
         <SDG:subgoalCorrelationFactor rdf:datatype="http://www.w3.org/2001/XMLSchema#decimal">{factor}</SDG:subgoalCorrelationFactor>
     </owl:NamedIndividual>
 """.format(cc = cc, from_target = from_target, to_target = to_target, factor = factor)
@@ -28,6 +30,8 @@ def generate_correlation_rdf(cc):
 		if rdf != "":
 			print(rdf)
 
-for k in correlations.indices.keys():
-	generate_correlation_rdf(k)
-
+if len(sys.argv) < 2:
+	for k in correlations.indices.keys():
+		generate_correlation_rdf(k)
+else:	
+	generate_correlation_rdf(sys.argv[1])
