1cd498bd3726ca8160be9a7d6def3b491c297c32
espandre <espandre@gmail.com>
2021-10-03 17:16:28 +0200

    Add correlations to ontology.

--- utils/correlation_generator/correlation_rdf.py
+++ utils/correlation_generator/correlation_rdf.py
@@ -17,10 +17,12 @@ def generate_correlation_rdf(cc):
 
     <owl:NamedIndividual rdf:about="http://www.semanticweb.org/aga/ontologies/2017/9/SDG#correlation.{cc}.{from_target}-{to_target}">
         <rdf:type rdf:resource="http://www.semanticweb.org/aga/ontologies/2017/9/SDG#SDGTargetCorrelation"/>
+        <SDG:subgoalCorrelationFrom rdf:resource="http://www.semanticweb.org/aga/ontologies/2017/9/SDG#{from_target}"/>
         <SDG:subgoalCorrelationFrom rdf:resource="http://metadata.un.org/sdg/{from_target}"/>
+        <SDG:subgoalCorrelationTo rdf:resource="http://www.semanticweb.org/aga/ontologies/2017/9/SDG#{to_target}"/>
         <SDG:subgoalCorrelationTo rdf:resource="http://metadata.un.org/sdg/{to_target}"/>
         <SDG:subgoalCorrelationCountry rdf:datatype="http://www.w3.org/2001/XMLSchema#string">{cc}</SDG:subgoalCorrelationCountry>
-        <SDG:subgoalCorrelationFactor rdf:datatype="http://www.w3.org/2001/XMLSchema#decimal">{factor}</SDG:subgoalCorrelationFactor>
+        <SDG:subgoalCorrelationFactor rdf:datatype="http://www.w3.org/2001/XMLSchema#double">{factor}</SDG:subgoalCorrelationFactor>
     </owl:NamedIndividual>
 """.format(cc = cc, from_target = from_target, to_target = to_target, factor = factor)
 
