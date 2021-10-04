1457939859aec6a961213fe964d8631ccc533a3d
Fredrik Busklein <fredrbus@stud.ntnu.no>
2021-10-04 18:12:00 +0200

    Added liveness- and loggedIn-probing endpoints

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,11 +3,20 @@ import { Router } from 'express';
 import ontologies from './ontologies';
 import authorization from './authorization';
 import data from './data';
+import { getLoginStatus } from 'database/login';
 
 const router = Router();
 
 router.use('/ontologies', ontologies);
 router.use('/auth', authorization);
 router.use('/data', data);
+
+router.use('/isAlive', (req, res) => {
+  res.send('true');
+});
+
+router.use('/isLoggedIn', (req, res) => {
+  res.send(getLoginStatus());
+});
 
 export default router;
