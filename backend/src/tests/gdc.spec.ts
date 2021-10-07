530fb6ee5bddd13f9b4db18157772f5aabfc580e
espandre <espandre@gmail.com>
2021-10-07 14:56:06 +0200

    Disable testing for now, will enable when the testing branch is merged.

--- backend/src/tests/gdc.spec.ts
+++ backend/src/tests/gdc.spec.ts
@@ -1,32 +1,34 @@
-import { assert } from 'chai';
-import getSimilarlySizedMunicipalities from './../database/getSimilarlySizedMunicipalities';
+/* eslint-disable no-restricted-syntax */
+
+// import { assert } from 'chai';
+// import getSimilarlySizedMunicipalities from '../database/getSimilarlySizedMunicipalities';
 
 describe('Similar municipalities', () => {
-	
-    const similarToTrondheim = await getSimilarlySizedMunicipalities("no.5001", 0.25);	
-	/*
-		name, code, population
-		1. "Tampere", "fi.837", 209535
-		2. "Turku", "fi.853", 175645
-		3. "Nijmegen", "nl.0268", 177321
-		4. "Eindhoven", "nl.0772", 235707
-		5. "Uppsala", "se.0380", 177074
-    */
-	const expectedSimilarToTrondheim = new Set([ "fi.837", "fi.853", "nl.0268", "nl.0772", "se.0380" ]);
+  /*
+  const similarToTrondheim = await getSimilarlySizedMunicipalities('no.5001', 0.25);
+  
+  // Expected output:
+	//	name, code, population
+	//	1. "Tampere", "fi.837", 209535
+	//	2. "Turku", "fi.853", 175645
+	//	3. "Nijmegen", "nl.0268", 177321
+	//	4. "Eindhoven", "nl.0772", 235707
+	//	5. "Uppsala", "se.0380", 177074
 
-    it('All similar municipalities are expected', () => {
-    	for (municipality of similarToTrondheim)
-    	{
-    		assert(expected.has(municipality.code));
-    	}
-    });
+  const expectedSimilarToTrondheim = new Set(['fi.837', 'fi.853', 'nl.0268', 'nl.0772', 'se.0380']);
 
-    it('No unexpected municipalities', () => {
-    	for (municipality of similarToTrondheim) 
-    	{
-    		expected.remove(municipality);
-    	}
+  it('All similar municipalities are expected', () => {
+    for (municipality of similarToTrondheim) {
+      assert(expectedSimilarToTrondheim.has(municipality.code));
+    }
+  });
 
-    	assert(expected.size == 0);
-    });
+  it('No unexpected municipalities', () => {
+    for (municipality of similarToTrondheim) {
+      expectedSimilarToTrondheim.remove(municipality);
+    }
+
+    assert(expectedSimilarToTrondheim.size === 0);
+  });
+*/
 });
