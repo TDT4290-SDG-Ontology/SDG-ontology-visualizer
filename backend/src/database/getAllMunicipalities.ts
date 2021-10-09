fc34647065e6f6f1bb76d19598faa73bc64f1a41
espandre <espandre@gmail.com>
2021-10-09 13:53:54 +0200

    Add endpoint for getting all municipalities.

---
+++ backend/src/database/getAllMunicipalities.ts
@@ -0,0 +1,8 @@
+import DB from './index';
+import getAllMunicipalities from './queries/getAllMunicipalities';
+
+// TODO: Remove any
+export default async (): Promise<any> => {
+  const query = getAllMunicipalities();
+  return DB.query(query, { transform: 'toJSON' }).then((resp) => resp.records);
+};
--- backend/src/routes/municipality.ts
+++ backend/src/routes/municipality.ts
@@ -3,6 +3,7 @@ import onError from './middleware/onError';
 import verifyDatabaseAccess from './middleware/verifyDatabaseAccess';
 
 import getSimilarlySizedMunicipalities from '../database/getSimilarlySizedMunicipalities';
+import getAllMunicipalities from '../database/getAllMunicipalities';
 
 const router = Router();
 
@@ -15,6 +16,17 @@ const findSimilar = async (req: Request, res: Response) => {
   }
 };
 
+const getAll = async (req: Request, res: Response) => {
+  try {
+    const resp = await getAllMunicipalities();
+    res.json(resp);
+  } catch (e: any) {
+    onError(e, req, res);
+  }
+};
+
+
 router.post('/similar', verifyDatabaseAccess, findSimilar);
+router.post('/all', verifyDatabaseAccess, getAll);
 
 export default router;
