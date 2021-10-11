0b623a6b6005f9a886484f4726b42156d7612a21
espandre <espandre@gmail.com>
2021-10-11 03:28:37 +0200

    Find N lowest performing indicators, and display them.

--- frontend/src/components/molecules/GDCView.tsx
+++ frontend/src/components/molecules/GDCView.tsx
@@ -12,14 +12,18 @@ type GDCViewProps = {
 const GDCView: React.FC<GDCViewProps> = (props: GDCViewProps) => {
   const [gdcInfo, setGDCInfo] = useState<GDCOutput>();
   const [indicatorArray, setIndicatorArray] = useState<Array<any>>();
+  const [worstIndicators, setWorstIndicators] = useState<Array<any>>();
 
   const { code, year } = props;
+  const WORST_COUNT = 5;
 
   const loadGDCOutput = async (muniCode: string, muniYear: number) => {
       const data = await getGDCOutput(muniCode, muniYear);
       setGDCInfo(data);
-      if (data !== undefined)
+      if (data !== undefined) {
         setIndicatorArray(Array.from(data.indicators));
+        setWorstIndicators(Array.from(data.indicators).sort((a, b) => a[1].score - b[1].score).slice(0, WORST_COUNT));
+      }
   };
 
   useEffect(() => {
@@ -71,7 +75,21 @@ const GDCView: React.FC<GDCViewProps> = (props: GDCViewProps) => {
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              <Text>Yobbo</Text>
+              <Accordion allowMultiple>
+                { worstIndicators && worstIndicators.map(([key, val]) => (
+                  <AccordionItem key={key}>
+                    <AccordionButton>
+                      <Box flex="1" textAlign="left">
+                        {key}
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                    <AccordionPanel>
+                      <Text>{val.score}</Text>
+                    </AccordionPanel>
+                  </AccordionItem>
+                ))}      
+              </Accordion>
             </AccordionPanel>
           </AccordionItem>
           { indicatorArray && indicatorArray.map(([key, val]) => (
